0667534cc6d40b0ba33952e9164acf551c561051
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-05-27 22:25:10 +1000

    Add ThirdyDaysInMelbourneCard composable

--- app/src/main/java/com/example/thirtydaysinmelbourne/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysinmelbourne/MainActivity.kt
@@ -4,15 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.ThirtyDaysInMelbourneTheme
-import com.example.thirtydaysinmelbourne.ui.theme.ThirtyDaysInMelbourneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,9 +45,39 @@ class MainActivity : ComponentActivity() {
 fun ThirtyDaysInMelbourne(modifier: Modifier = Modifier) {
 }
 
+@Composable
+fun ThirdyDaysInMelbourneCard(modifier: Modifier = Modifier) {
+    ElevatedCard(
+        modifier = modifier
+            .height(300.dp)
+    ) {
+        Column (modifier = Modifier.padding(8.dp)) {
+            Image(
+                painter = painterResource(R.drawable.flinders_street_station),
+                contentDescription = stringResource(R.string.flinders_street_station_title),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .padding(bottom = 8.dp)
+                    .clip(shape = MaterialTheme.shapes.medium)
+            )
+            Text(
+                text = stringResource(R.string.flinders_street_station_title),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = stringResource(R.string.flinders_street_station_desc),
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
 @Preview
 @Composable
 fun ThirtyDaysInMelbournePreview() {
     ThirtyDaysInMelbourneTheme {
+        ThirdyDaysInMelbourneCard()
     }
 }
