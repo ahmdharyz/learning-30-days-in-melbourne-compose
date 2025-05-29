cbc8026ce97815474e12fc3cfdf33c4d7fbf8b49
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-05-29 21:28:17 +1000

    Update text style for TopAppBar text

--- app/src/main/java/com/example/thirtydaysinmelbourne/MainActivity.kt
+++ app/src/main/java/com/example/thirtydaysinmelbourne/MainActivity.kt
@@ -6,10 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -26,7 +23,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
@@ -78,7 +74,7 @@ fun ThirtyDaysInMelbourneTopAppBar(modifier: Modifier = Modifier) {
         title = {
             Text(
                 text = stringResource(R.string.app_name),
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.headlineMedium
             )
         }
     )
--- app/src/main/java/com/example/thirtydaysinmelbourne/ui/theme/Type.kt
+++ app/src/main/java/com/example/thirtydaysinmelbourne/ui/theme/Type.kt
@@ -2,8 +2,16 @@ package com.example.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.thirtydaysinmelbourne.R
 
-val AppTypography = Typography()
+val lexenddeca = FontFamily(Font(R.font.lexenddeca_variablefont_wght))
+
+val AppTypography = Typography  (
+    headlineMedium = Typography().headlineMedium.copy(
+        fontFamily = lexenddeca
+    )
+)
